afd5f3a739bbb289a420d56b961c9458d8725e64
justgogame <justnite@gmail.com>
2025-01-07 17:31:41 -0500

    Mui Typography

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,7 +1,62 @@
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Rating from '@mui/material/Rating';
+import { AccessTime } from '@mui/icons-material';
 
 const TourCard = () => {
-  return <Paper elevation="3">Hello</Paper>;
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={3}>
+        <img
+          src="https://justinpluslauren.com/wp-content/uploads/2015/01/Niagara-Falls-Winter-Title-1-728x364.jpg.webp"
+          alt=""
+          className="img"
+        />
+        <Box paddingX={1}>
+          <Typography variant="subtitle1" component="h2">
+            Immerse into the Falls
+          </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center'
+            }}>
+            <AccessTime sx={{ width: 12.5 }} />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              5 hours
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center'
+            }}
+            marginTop={3}>
+            <Rating
+              name="read-only"
+              value={4.5}
+              readOnly
+              precision={0.5}
+              size="small"
+            />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              4.5
+            </Typography>
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              (655 revies)
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="bodyh6" component="h3" marginTop={0}>
+              From C $147
+            </Typography>
+          </Box>
+        </Box>
+      </Paper>
+    </Grid>
+  );
 };
 
 export default TourCard;
