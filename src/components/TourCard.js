0327e8490efd1b77b1c5b81e54a987d206e4a0cb
justgogame <justnite@gmail.com>
2025-01-07 17:59:29 -0500

    Custom Themes with Providers

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -4,57 +4,81 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Rating from '@mui/material/Rating';
 import { AccessTime } from '@mui/icons-material';
+import { createTheme, ThemeProvider } from '@mui/material';
 
-const TourCard = () => {
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      varients: [
+        {
+          props: {
+            variant: 'body2'
+          },
+          style: {
+            fontSize: 11
+          }
+        },
+        {
+          props: {
+            variant: 'body3'
+          },
+          style: {
+            fontSize: 9
+          }
+        }
+      ]
+    }
+  }
+});
+
+const TourCard = ({ tour }) => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img
-          src="https://justinpluslauren.com/wp-content/uploads/2015/01/Niagara-Falls-Winter-Title-1-728x364.jpg.webp"
-          alt=""
-          className="img"
-        />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Immerse into the Falls
-          </Typography>
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center'
-            }}>
-            <AccessTime sx={{ width: 12.5 }} />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img src={tour.image} alt="" className="img" />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              {tour.name}
             </Typography>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center'
+              }}>
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                {tour.duration} hours
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center'
+              }}
+              marginTop={3}>
+              <Rating
+                name="read-only"
+                value={tour.rating}
+                readOnly
+                precision={0.5}
+                size="small"
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                {tour.rating}
+              </Typography>
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                ({tour.numberOfReviews} reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="bodyh6" component="h3" marginTop={0}>
+                From C ${tour.price}
+              </Typography>
+            </Box>
           </Box>
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center'
-            }}
-            marginTop={3}>
-            <Rating
-              name="read-only"
-              value={4.5}
-              readOnly
-              precision={0.5}
-              size="small"
-            />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              4.5
-            </Typography>
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              (655 revies)
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="bodyh6" component="h3" marginTop={0}>
-              From C $147
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
